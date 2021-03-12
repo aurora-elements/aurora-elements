f2464e7755346dbcaf96e8a6e766fd2f22b574f6
Marcus Kramer <marcus.kramer@wissenswerft.net>
2021-03-12 20:34:00 +0100

    add styling

--- src/components/visualisation/CodeMirror/CodeMirror.js
+++ src/components/visualisation/CodeMirror/CodeMirror.js
@@ -35,9 +35,35 @@ class AuroraCodeMirror extends LitElement {
     firstUpdated() {
         super.firstUpdated();
 
+        const head = document.getElementsByTagName('head')[0];
+
+        if(document.getElementById('codemirror') == null) {
+            const style = document.createElement('style');
+            style.id= 'codemirror';
+            style.innerHTML = `
+                .hljs{display:block;overflow-x:auto;padding:.5em;color:#333;background:#f8f8f8}
+                .hljs-comment,.hljs-quote{color:#998;font-style:italic}
+                .hljs-keyword,.hljs-selector-tag,.hljs-subst{color:#333;font-weight:700}
+                .hljs-literal,.hljs-number,.hljs-tag .hljs-attr,.hljs-template-variable,.hljs-variable{color:teal}
+                .hljs-doctag,.hljs-string{color:#d14}
+                .hljs-section,.hljs-selector-id,.hljs-title{color:#900;font-weight:700}
+                .hljs-subst{font-weight:400}
+                .hljs-class .hljs-title,.hljs-type{color:#458;font-weight:700}
+                .hljs-attribute,.hljs-name,.hljs-tag{color:navy;font-weight:400}
+                .hljs-link,.hljs-regexp{color:#009926}
+                .hljs-bullet,.hljs-symbol{color:#990073}
+                .hljs-built_in,.hljs-builtin-name{color:#0086b3}
+                .hljs-meta{color:#999;font-weight:700}
+                .hljs-deletion{background:#fdd}
+                .hljs-addition{background:#dfd}
+                .hljs-emphasis{font-style:italic}
+                .hljs-strong{font-weight:700}`;
+            head.append(style);
+        }
+
         const slot =        this.root.querySelector('slot');
         const content =     slot.assignedNodes()[0].textContent;
-        const copyButton = this.root.getElementById('copyButton');
+        const copyButton =  this.root.getElementById('copyButton');
         const copySuccess = this.root.getElementById('copy-success');
 
         document.querySelectorAll('aurora-code-mirror').forEach(block => {
