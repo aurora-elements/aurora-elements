061e125957ca286c2f424a7024856d61c6ce3eea
Marcus Kramer <marcus.kramer@wissenswerft.net>
2021-04-06 19:20:57 +0200

    testing config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
@@ -11,22 +12,18 @@ module.exports = {
     showcase: './src/showcase.js'
   },
   output: {
+    clean: true,
     publicPath: '/'
  },
- module: {
-    rules: [
-        {
-            test: /\.css$/,
-            use: ["style-loader", "css-loader"]
-        }
-    ]
-},
   devtool: "inline-source-map",
   plugins: [
-   /* new CleanWebpackPlugin({ 
+    new CleanWebpackPlugin({ 
         cleanStaleWebpackAssets: false,
         patterns: [{from: './src/showcase', to: 'showcase'}] 
-    }),*/
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{from: './src/showcase', to: 'showcase'}]
+    }),
     new HtmlWebpackPlugin({
         chunks: ['showcase'],
         template: './src/showcase.html'
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "production",
@@ -15,6 +16,9 @@ module.exports = {
     new HtmlWebpackPlugin({
         chunks: ['showcase'],
         template: './src/showcase.html'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{from: './src/showcase', to: 'showcase'}]
     }),
   ],
   output: {
