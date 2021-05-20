9a7ba7d0f53f9fd84c69fab3dfd0192c0d5e4d9f
Marcus Kramer <marcus.kramer@wissenswerft.net>
2021-05-20 18:40:40 +0200

    remove comment

--- src/foundations/directives/spo/spo.p2f.creator.url.ts
+++ src/foundations/directives/spo/spo.p2f.creator.url.ts
@@ -1,5 +1,3 @@
-// https://creator.page2flip.devdock.space.one/wizard/hotspot-editor-standalone/1?p=https:%2F%2Fkreativburschen.customer.space.one%2Fapi%2Fscope%2Fwolfenbuettlerschaufenster%2Fasset%2F3231%2Fformat%2Fp2fdocument%2Fcontent%2F
-
 export function spoP2fCreatorUrl(
     creatorUrl: string = 'https://creator.page2flip.devdock.space.one/wizard/hotspot-editor-standalone/1?p=', 
     apiUrl: string,
