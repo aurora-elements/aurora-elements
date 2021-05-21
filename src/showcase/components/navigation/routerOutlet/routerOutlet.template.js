b04493ef26456674666d53158eab2e79a22f32f6
Marcus Kramer <marcus.kramer@wissenswerft.net>
2021-05-21 18:50:02 +0200

    bug fix

--- src/showcase/components/navigation/routerOutlet/routerOutlet.template.js
+++ src/showcase/components/navigation/routerOutlet/routerOutlet.template.js
@@ -5,11 +5,8 @@ export function template() {
       <welcome-page route="welcome"></welcome-page>
       <whatsnew-page route="whatsnew"></whatsnew-page>
       <imprint-page route="imprint"></imprint-page>
-<<<<<<< HEAD
       <card-page route="card"></card-page>
-=======
       <wizard-page route="wizard"></wizard-page>
->>>>>>> feature/switchToLit
       <not-found-page route="not-found"></not-found-page>
     `
 }
