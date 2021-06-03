fbbe5d29ee73f3c00dc831ffc9f0cacd359d7baa
Marcus Kramer <marcus.kramer@wissenswerft.net>
2021-06-03 19:36:59 +0200

    bug fix 2

--- webpack.prod.js
+++ webpack.prod.js
@@ -14,6 +14,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
         test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
@@ -78,6 +83,9 @@ module.exports = {
       ]
     }),
   ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
