90cdafbb2b74739fa68bda1ae13f2e4b0e43c9f3
marcus.kramer <hgc5UdoX>
2021-06-11 15:17:47 +0200

    testing config 2

--- webpack.prod.js
+++ webpack.prod.js
@@ -13,15 +13,6 @@ module.exports = {
     showcase: './src/showcase.ts'
   },
   module: {
-    loaders: [
-      // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-      {test: /\.tsx?$/, loader: "ts-loader"}
-  ],
-
-  preLoaders: [
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      {test: /\.js$/, loader: "source-map-loader"}
-  ],
     rules: [
       {
         test: /\.tsx?$/,
