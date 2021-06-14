283fe509d63fd7b47a28f3a5dbe7ebb6719c1e9c
marcus.kramer <hgc5UdoX>
2021-06-14 13:32:45 +0200

    add styling to data table component

--- src/elements/visualisation/DataTable/DataTable.element.ts
+++ src/elements/visualisation/DataTable/DataTable.element.ts
@@ -2,10 +2,74 @@ import { LitElement, html, css, nothing } from 'lit';
 import { until } from 'lit/directives/until';
 import { repeat } from 'lit/directives/repeat.js';
 import { unsafeHTML } from 'lit/directives/unsafe-html.js';
-import { customElement, property, query, queryAssignedNodes } from 'lit/decorators.js';
+import { customElement, property } from 'lit/decorators.js';
 import { locale } from '../../../functionalities/directives/translater.directive';
 
-import { styles } from './DataTableStyles.js';
+const styles = css`
+    :host {
+        display: block;
+    }
+    table {
+        box-sizing: border-box;
+        border-collapse: separate;
+        border-spacing: 0;
+        width: 100%;
+        border-bottom: 1px solid #e0e6ed;
+        text-align: justify;
+    }
+    table thead th {
+        box-sizing: border-box;
+        font-size: 80%;
+        line-height: 1.3;
+        min-height: 12px;
+        padding: 10px 20px;
+        cursor: default; 
+        color: #9c9c9c;
+        white-space: nowrap;
+    }
+    table thead th:first-child {
+        padding: 10px 20px 10px 40px;
+    }
+    table thead th:last-child {
+        padding: 10px 40px 10px 20px;
+    }
+    table tbody {
+        border-width: 1px;
+        border: none;
+        vertical-align: top;
+        overflow: auto;
+    }
+    table strong {
+        color: #484848;
+        white-space: nowrap;
+    }
+    tbody tr {
+        border-top:none;
+        cursor: auto;
+    }
+    tr td {
+        border-left: none;
+        border-right: none;
+        border-top: 1px solid #e0e6ed;
+        font-weight: 400;
+        line-height: 1.5;
+        padding: 14px 20px;
+        min-height: 20px;
+        color: #9c9c9c;
+    }
+    td ul {
+        margin: 0;
+        padding: 0;
+        list-style-position: inside;
+        list-style-type: square;
+    }
+    tr td:first-child {
+        padding: 14px 20px 14px 40px;
+    }
+    tr td:last-child {
+        padding: 14px 40px 14px 20px;
+    }
+`;
 
 const tableLoadingTemplate = () => html`
     <tr part="tr-loading">
